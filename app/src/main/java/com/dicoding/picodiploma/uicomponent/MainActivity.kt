bc58314293437fc10f4df5c9330741d861c22c83
yudhayunantop <yudha.yunanto.2010@gmail.com>
2020-10-20 21:44:18 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/uicomponent/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dicoding.picodiploma.uicomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgBtn = findViewById<ImageButton>(R.id.imgButtonAdd)
+        imgBtn.setOnClickListener({
+            Toast.makeText(this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        })
+    }
+}
